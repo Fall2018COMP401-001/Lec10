215470c0391a5d9cd57a4ac71546334ade08a80f
Ketan Mayer-Patel <kmp@cs.unc.edu>
2018-09-27 11:23:25 -0400

    Init commit.

---
+++ src/lec10/v3/AnimalImpl.java
@@ -0,0 +1,8 @@
+package lec10.v3;
+
+public class AnimalImpl {
+
+	protected Point3D location;
+	protected int id;
+
+}
---
+++ src/lec10/v6/Bear.java
@@ -0,0 +1,6 @@
+package lec10.v6;
+
+public interface Bear extends Animal {
+
+	public String getColor();
+}
